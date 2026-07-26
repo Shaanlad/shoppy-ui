6d140c619eb9f50a971e05f6140eebcf9c71651f
Shantanu Lad <shaan.lad@gmail.com>
2026-07-26 22:31:13 +0530

    Updated Product Logic for Image Support on UI

--- app/products/product.tsx
+++ app/products/product.tsx
@@ -1,5 +1,7 @@
-import { Card, Typography } from "@mui/material";
+import { Card, Stack, Typography } from "@mui/material";
 import { Product as IProduct } from "./interfaces/product.interface";
+import Image from "next/image";
+import { API_URL } from "../common/constants/api";
 
 interface ProductProps {
     product: IProduct
@@ -8,15 +10,29 @@ interface ProductProps {
 export default function Product({product}: ProductProps) {
     return (
         <Card className="p-4" elevation={0} sx={{ bgcolor: "white", border: "1.5px solid #e0e0e0", borderRadius: "8px" }}>
-            <Typography variant="h5" component="h5" gutterBottom>
+            <Stack gap={3}>
+                <Typography variant="h5" component="h5" gutterBottom>
                 {product.name}
-            </Typography>
-            <Typography variant="body1" gutterBottom>
-                {product.description}
-            </Typography>
-            <Typography variant="h6" color="textSecondary">
-                ${product.price.toFixed(2)}
-            </Typography>
+                </Typography>
+                {
+                product.imageExists && (
+                    <Image 
+                        src={`${API_URL}/products/${product.id}.jpeg`}
+                        alt={product.name}
+                        width="0"
+                        height="0"  
+                        className="w-full h-auto mb-4"      
+                        sizes="100vw"       
+                    />
+                    )
+                }
+                <Typography variant="body1" gutterBottom>
+                    {product.description}
+                </Typography>
+                <Typography variant="h6" color="textSecondary">
+                    ${product.price.toFixed(2)}
+                </Typography>
+            </Stack>
         </Card>
     )
 }
--- app/products/products.tsx
+++ app/products/products.tsx
@@ -7,7 +7,7 @@ export default async function Products() {
     console.log("products >> ", products);
 
     return (
-         <Grid2 container spacing={3} >
+         <Grid2 container spacing={3} sx={{height: "85vh", overflowY: "scroll"}}>
             {products.map((product) => (
                 <Grid2 key={product.id} size={{ xs: 12, sm: 6, lg: 4 }}>
                     <Product product={product} />
